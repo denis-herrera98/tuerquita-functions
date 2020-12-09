a3f4a6e567d0c5c57a09dbd3d78d9fa8eee7f18e
Denis  Herrera Torres <denis.herrera@ucrso.info>
2020-12-09 02:33:13 -0600

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,14 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+    const original = req.query.text;
+    //     // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
+
